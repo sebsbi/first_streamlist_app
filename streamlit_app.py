1d0a3ac79ff7446cb0e1740d7cfb3d75cde906e7
sebsbi <144684884+sebsbi@users.noreply.github.com>
2023-09-12 11:18:58 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,6 @@ streamlit.text('🐔 Hard Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-
 #import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -28,38 +27,3 @@ def get_fruityvice_data (this_fruit_choice):
       fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +this_fruit_choice)
       fruityvice_normalized = pandas.json_normalize(fruityvice_response.json()
 return fruityvice_normalized
-
-# New section to display fruityvice api response
-streamlit.header ('Fruityvice Fruit Advice!')
-try : 
-      fruit_choice = streamlit.text.input('What fruit would you like information about?')
-      if not fruit_choice:
-      streamlit.error("Please select a fruit to get information.")
-      else :
-      back_from_function = get_fruityvice_data(fruit_choice)
-      streamlit.dataframe(back_from_function)
-
-#import snowflake.connector
-streamlit.header("The fruit load list contains:")
-#Snowflake-related functions
-def get_fruit_load_list():
-      with my_cnx.cursor() as my_cur:
-      my_cur.execute("select * from fruit_load_list")
-return my_cur.fetchall()
-
-#Add a button to load the fruit
-if streamlit.button('Get Fruit Load List'):
-      my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-      my_data_rows = get_fruit_load_list()
-      streamlit.dataframe(my_data_rows)
-
-# don't run anything pas here while we troubleshoot
-streamlit.stop()
-
-# Allow the end user to add a fruit to the list
-add_my_fruit = streamlit.text_input('What fruit would you like to add?')
-streamlit.write('Thanks for adding ' , add_my_fruit)
-      
-
-# This will work correctly, but just go with it for now
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
