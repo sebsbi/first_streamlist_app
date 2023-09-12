9524e2665ca1c1fa149e4ad05254b059aaf48b6e
sebsbi <144684884+sebsbi@users.noreply.github.com>
2023-09-12 12:53:30 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,9 @@ return fruityvice_normalized
 streamlit.header("Fruityvice Fruit Advice!")
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
-if not  fruit_choice:
+ if not fruit_choice:
+  streamlit.error("Please select a fruit to get information.")
+else 
   back_from_function = get_fruityvice_data(fruit_choice)
   streamlit.dataframe(back_from_function)
 
@@ -60,7 +62,7 @@ streamlit.dataframe(my_data_rows)
 #Allow the end user to add a fruit to the list
 def insert_row_snowflake(new_fruit):
  with my_cnx.cursor() as my_cur:
-  my_cur.execute("insert into fruit_load_list values ('" +  "jackfruit" + "papaya"+ "guava" + "kiwi" + "')")
+  my_cur.execute("insert into fruit_load_list values ('from streamlit')")
   return "Thanks for adding " + new_fruit
   
  add_my_fruit = streamlit.text_input('What fruit would you like to add?')
